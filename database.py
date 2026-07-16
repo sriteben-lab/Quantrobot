4403cd98e81641ff9f712efcb9447ebac247a3bf
sriteben-lab <sriteben@gmail.com>
2026-07-16 01:55:05 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -28,3 +28,58 @@ def create_tables():
 
     conn.commit()
     conn.close()
+    
+    
+    def user_exists(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT user_id FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    user = cursor.fetchone()
+    conn.close()
+
+    return user is not None
+
+
+def add_user(user_id, full_name, email, phone, country):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO users(
+            user_id,
+            full_name,
+            email,
+            phone,
+            country
+        )
+        VALUES(?,?,?,?,?)
+    """, (
+        user_id,
+        full_name,
+        email,
+        phone,
+        country
+    ))
+
+    conn.commit()
+    conn.close()
+
+
+def get_user(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE user_id=?",
+        (user_id,)
+    )
+
+    user = cursor.fetchone()
+    conn.close()
+
+    return user
