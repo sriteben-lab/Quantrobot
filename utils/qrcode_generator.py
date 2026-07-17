b427a6bb258a560069cd51f5ad3f934ebee326f4
sriteben-lab <sriteben@gmail.com>
2026-07-17 23:10:26 +0100

    Update qrcode_generator.py

--- utils/qrcode_generator.py
+++ utils/qrcode_generator.py
@@ -2,14 +2,42 @@ import qrcode
 import tempfile
 
 
-def generate_qr(address):
-    qr = qrcode.make(address)
+def generate_qr(network, address, amount):
+
+    if network == "BTC":
+        qr_data = f"bitcoin:{address}?amount={amount:.8f}"
+
+    elif network == "ETH":
+        qr_data = f"ethereum:{address}?value={amount:.8f}"
+
+    elif network == "USDT TRC20":
+        qr_data = address
+
+    elif network == "USDT ERC20":
+        qr_data = f"ethereum:{address}"
+
+    elif network == "USDC ERC20":
+        qr_data = f"ethereum:{address}"
+
+    else:
+        qr_data = address
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(qr_data)
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="black", back_color="white")
 
     temp = tempfile.NamedTemporaryFile(
         suffix=".png",
         delete=False
     )
 
-    qr.save(temp.name)
+    image.save(temp.name)
 
     return temp.name
