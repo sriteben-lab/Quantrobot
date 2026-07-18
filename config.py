bb2775cc7979e21edbbf8eb7ac511f7d040609de
sriteben-lab <sriteben@gmail.com>
2026-07-18 06:56:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-# Telegram Bot Token (set in Railway environment variables)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Your Telegram User ID
 ADMIN_ID = 8160417866
+ADMIN_CHAT_ID = ADMIN_ID
